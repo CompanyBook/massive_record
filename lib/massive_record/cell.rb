065a577c3d053ce9367a7576fa9ad732c686dfb6
Vincent Peres <vincent.peres@gmail.com>
2010-11-26 10:17:03 +0100

    Use Marshal serialization instead of JSON to store data in HBase

--- lib/massive_record/cell.rb
+++ lib/massive_record/cell.rb
@@ -13,50 +13,15 @@ module MassiveRecord
     end
     
     def value
-      @value.is_a?(String) ? self.class.convert_string(@value) : @value
-    end
-    
-    def self.convert_string(str)
-      str.to_s.force_encoding(Encoding::UTF_8)
-    end
-    
-    def self.convert_hash(hash)
-      hash.inject({}) do |h,(k,v)|
-        if v.is_a?(String)
-          h[k] = convert_string(v)
-        elsif v.is_a?(Hash)
-          h[k] = convert_hash(v)
-        elsif v.is_a?(Array)
-          h[k] = convert_array(v)
-        end
-        h
-      end      
-    end
-    
-    def self.convert_array(ar)
-      ar.collect do |k|
-        if k.is_a?(String)
-          convert_string(k)
-        elsif k.is_a?(Hash)
-          convert_hash(k)
-        elsif k.is_a?(Array)
-          convert_array(k)
-        end
-      end
+      @value.is_a?(String) ? @value.to_s.force_encoding(Encoding::UTF_8) : @value
     end
     
     def deserialize_value
-      @value.is_a?(String) ? JSON.parse(@value) : @value
+      @value.is_a?(String) ? Marshal.load(@value) : @value
     end
     
     def self.serialize_value(v)
-      if v.is_a?(String)
-        v
-      elsif v.is_a?(Hash)
-        v.to_json
-      elsif v.is_a?(Array)
-        v.to_json
-      end
+      v.is_a?(String) ? v : Marshal.dump(v)
     end
     
     def serialize_value(v)
