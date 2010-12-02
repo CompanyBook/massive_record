3dcd6be6fb83570cd1329772e990af89dd49db0d
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-02 09:46:32 +0100

    Took use of ActiveSupport::Concern in naming module.
    ..just includes some common behaviour like extend included class with
    ClassMethods module etc.

--- lib/massive_record/orm/naming.rb
+++ lib/massive_record/orm/naming.rb
@@ -1,17 +1,13 @@
 module MassiveRecord
   module ORM
     module Naming
-      
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
+      extend ActiveSupport::Concern 
       
       module ClassMethods
         def table_name
           @table_name ||= self.to_s.demodulize.underscore.pluralize
         end
       end
-      
     end
   end
 end
