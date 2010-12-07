08bb407772bcedc7ecb0eb6a7ec5f5792acfb08f
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-07 17:05:10 +0100

    Commented out a connection mock.
    Have to do this conditionally / expressive at the
    tests depended on it.
    
    Alot of things will fail now ;)

--- spec/orm/spec_helper.rb
+++ spec/orm/spec_helper.rb
@@ -1,8 +1,8 @@
 Rspec.configure do |config|
   config.before(:each) do
-    mock_connection = mock(MassiveRecord::Wrapper::Connection, :open => true)
-    MassiveRecord::Wrapper::Connection.stub(:new).and_return(mock_connection)
-    MassiveRecord::ORM::Base.connection_configuration = {:host => "foo", :port => 9001}
+    #mock_connection = mock(MassiveRecord::Wrapper::Connection, :open => true)
+    #MassiveRecord::Wrapper::Connection.stub(:new).and_return(mock_connection)
+    #MassiveRecord::ORM::Base.connection_configuration = {:host => "foo", :port => 9001}
   end
 
   config.after(:each) do
