4ced1cf7ca0cf2b5e968e4f1f9991ca74743a959
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-08 16:26:24 +0100

    Mass assignment of attribute now raises error if it encounters an unkown attribute.

--- spec/orm/cases/attribute_methods_spec.rb
+++ spec/orm/cases/attribute_methods_spec.rb
@@ -3,7 +3,7 @@ require 'orm/models/basic'
 
 describe "attribute methods" do
   before do
-    @model = Basic.new :foo => :bar
+    @model = Basic.new :foo => :bar, :ding => :dong
   end
 
   it "should define reader method" do
@@ -28,6 +28,16 @@ describe "attribute methods" do
     it "should simply return if incomming value is not a hash" do
       @model.attributes = "FOO => BAR"
       @model.attributes.keys.should include("foo")
+    end
+
+    it "should mass assign attributes" do
+      @model.attributes = {:foo => "foo", :ding => "ding"}
+      @model.foo.should == "foo"
+      @model.ding.should == "ding"
+    end
+
+    it "should raise an error if we encounter an unkown attribute" do
+      lambda { @model.attributes = {:unkown => "foo"} }.should raise_error MassiveRecord::ORM::UnkownAttributeError
     end
   end
 end
