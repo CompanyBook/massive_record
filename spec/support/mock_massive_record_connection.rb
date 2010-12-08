059617dd4b3e9e3c65487c12cd7f6a92c3853f56
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-08 13:51:08 +0100

    Removed injection of find method on ORM::Base.table.
    The find method is now injected into Wrapper::Table.new
    method calls instead when MockMassiveRecordConnection is
    included into spec blocks.

--- spec/support/mock_massive_record_connection.rb
+++ spec/support/mock_massive_record_connection.rb
@@ -6,9 +6,25 @@ module MockMassiveRecordConnection
   def self.included(base)
     base.class_eval do
       before do
+        # Setting up expected connection configuration, or else an error will be raised
+        MassiveRecord::ORM::Base.connection_configuration = {:host => "foo", :port => 9001}
+
+        # Setting up a mock connection when asked for new
         mock_connection = mock(MassiveRecord::Wrapper::Connection, :open => true)
         MassiveRecord::Wrapper::Connection.stub(:new).and_return(mock_connection)
-        MassiveRecord::ORM::Base.connection_configuration = {:host => "foo", :port => 9001}
+
+        # Inject find method on tables so that we don't need to go through with
+        # the actual call to the database.
+        new_table_method = MassiveRecord::Wrapper::Table.method(:new)
+        MassiveRecord::Wrapper::Table.stub!(:new) do |*args|
+          table = new_table_method.call(*args)
+          # Defines a dummy find method which simply returns a hash where id is set to the first
+          # argument (Like Person.find(1)).
+          def table.find(*args)
+            {:id => args[0]}
+          end
+          table
+        end
       end
 
       after do
