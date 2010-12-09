dc18ce3336a440f327b270d777e2308e4a7fecce
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-09 12:50:33 +0100

    Refactored the spec support files a bit.
    Using Concern and making connection before hooks
    reusable.

--- spec/support/connection_helpers.rb
+++ spec/support/connection_helpers.rb
@@ -1,29 +1,40 @@
-module CreatePersonBeforeEach
-  def self.included(base)
-    base.class_eval do
-      before(:all) do
+module SetUpHbaseConnectionBeforeAll
+  extend ActiveSupport::Concern
+
+  included do
+    before(:all) do
+      unless @connection
         @connection_configuration = {:host => MR_CONFIG['host'], :port => MR_CONFIG['port']}
         @connection = MassiveRecord::Wrapper::Connection.new(@connection_configuration)
         @connection.open
       end
+    end
+  end
+end
 
-      before do
-        Person.stub!(:table_name).and_return(MR_CONFIG['table'])
-        Person.connection_configuration = @connection_configuration
-        @table = MassiveRecord::Wrapper::Table.new(@connection, Person.table_name)
-        @table.column_families.create(:info)
-        @table.save
-        
-        @row = MassiveRecord::Wrapper::Row.new
-        @row.id = "ID1"
-        @row.values = {:info => {:name => "John Doe", :email => "john@base.com", :age => "20"}}
-        @row.table = @table
-        @row.save
-      end
 
-      after do
-        @table.destroy 
-      end
+module CreatePersonBeforeEach
+  extend ActiveSupport::Concern
+
+  included do
+    include SetUpHbaseConnectionBeforeAll
+
+    before do
+      Person.stub!(:table_name).and_return(MR_CONFIG['table'])
+      Person.connection_configuration = @connection_configuration
+      @table = MassiveRecord::Wrapper::Table.new(@connection, Person.table_name)
+      @table.column_families.create(:info)
+      @table.save
+      
+      @row = MassiveRecord::Wrapper::Row.new
+      @row.id = "ID1"
+      @row.values = {:info => {:name => "John Doe", :email => "john@base.com", :age => "20"}}
+      @row.table = @table
+      @row.save
+    end
+
+    after do
+      @table.destroy 
     end
   end
 end
--- spec/support/mock_massive_record_connection.rb
+++ spec/support/mock_massive_record_connection.rb
@@ -3,37 +3,37 @@
 # skip the actual database when it's not needed.
 #
 module MockMassiveRecordConnection
-  def self.included(base)
-    base.class_eval do
-      before do
-        # Setting up expected connection configuration, or else an error will be raised
-        MassiveRecord::ORM::Base.connection_configuration = {:host => "foo", :port => 9001}
+  extend ActiveSupport::Concern
 
-        # Setting up a mock connection when asked for new
-        mock_connection = mock(MassiveRecord::Wrapper::Connection, :open => true)
-        MassiveRecord::Wrapper::Connection.stub(:new).and_return(mock_connection)
+  included do
+    before do
+      # Setting up expected connection configuration, or else an error will be raised
+      MassiveRecord::ORM::Base.connection_configuration = {:host => "foo", :port => 9001}
 
-        # Inject find method on tables so that we don't need to go through with
-        # the actual call to the database.
-        new_table_method = MassiveRecord::Wrapper::Table.method(:new)
-        MassiveRecord::Wrapper::Table.stub!(:new) do |*args|
-          table = new_table_method.call(*args)
-          # Defines a dummy find method which simply returns a hash where id is set to the first
-          # argument (Like Person.find(1)).
-          def table.find(*args)
-            row = MassiveRecord::Wrapper::Row.new
-            row.id = args[0]
-            row.values = { }
-            row
-          end
-          table
+      # Setting up a mock connection when asked for new
+      mock_connection = mock(MassiveRecord::Wrapper::Connection, :open => true)
+      MassiveRecord::Wrapper::Connection.stub(:new).and_return(mock_connection)
+
+      # Inject find method on tables so that we don't need to go through with
+      # the actual call to the database.
+      new_table_method = MassiveRecord::Wrapper::Table.method(:new)
+      MassiveRecord::Wrapper::Table.stub!(:new) do |*args|
+        table = new_table_method.call(*args)
+        # Defines a dummy find method which simply returns a hash where id is set to the first
+        # argument (Like Person.find(1)).
+        def table.find(*args)
+          row = MassiveRecord::Wrapper::Row.new
+          row.id = args[0]
+          row.values = {}
+          row
         end
+        table
       end
+    end
 
-      after do
-        MassiveRecord::ORM::Base.descendants.each { |klass| klass.unmemoize_all }
-        MassiveRecord::ORM::Base.reset_connection!
-      end
+    after do
+      MassiveRecord::ORM::Base.descendants.each { |klass| klass.unmemoize_all }
+      MassiveRecord::ORM::Base.reset_connection!
     end
   end
 end
