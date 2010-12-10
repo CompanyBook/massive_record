f8c417d54b5af3141b24aa4e7e4287228f69a166
Vincent Peres <vincent.peres@gmail.com>
2010-12-10 14:02:23 +0100

    Add autoload boolean to the column family

--- lib/massive_record/orm/column_family.rb
+++ lib/massive_record/orm/column_family.rb
@@ -2,11 +2,12 @@ module MassiveRecord
   module ORM
     class ColumnFamily
       
-      attr_reader :name
+      attr_reader :name, :autoload
       
       def initialize(name, &block)
         @fields = []
         @name = name
+        @autoload = false
         instance_eval &block
       end
       
@@ -18,6 +19,10 @@ module MassiveRecord
       
       def fields
         @fields.inject(Fields.new){|h, (field)| h[field.name.to_s] = field; h}
+      end
+      
+      def autoload
+        @autoload = true
       end
       
     end
--- spec/orm/cases/column_family_spec.rb
+++ spec/orm/cases/column_family_spec.rb
@@ -1,31 +1,58 @@
 require 'spec_helper'
 
-describe "column_family" do
-  before do
-    @subject = MassiveRecord::ORM::ColumnFamily.new(:info) do
-      field :first_name
-      field :last_name
-    end
-  end
+shared_examples_for "Column Family description" do
   
   it "should have a name" do
     @subject.name.should == :info
   end
   
-  it "should have a collection a fields" do
-    @subject.fields.should be_a_kind_of(MassiveRecord::ORM::Fields)
-  end
+end
+
+describe "column_family" do
   
   describe "fields" do
+    
+    before do
+      @subject = MassiveRecord::ORM::ColumnFamily.new(:info) do
+        field :first_name
+        field :last_name
+      end
+    end
   
-    it "should have a two keys made of the name" do
-      @subject.fields.keys.sort == ["first_name", "last_name"].to_s
+    it_should_behave_like "Column Family description"
+  
+    it "should have a collection a fields" do
+      @subject.fields.should be_a_kind_of(MassiveRecord::ORM::Fields)
+    end
+
+    describe "field" do
+
+      it "should have a two keys made of the name" do
+        @subject.fields.keys.sort == ["first_name", "last_name"].to_s
+      end
+
+      it "should have a Field object" do
+        @subject.fields["first_name"].should be_a_kind_of(MassiveRecord::ORM::Field)
+      end
+
     end
     
-    it "should have a Field object" do
-      @subject.fields["first_name"].should be_a_kind_of(MassiveRecord::ORM::Field)
-    end
+  end
   
+  describe "autoload" do
+    
+    before do
+      @subject = MassiveRecord::ORM::ColumnFamily.new(:info) do
+        autoload
+      end
+    end
+    
+    it_should_behave_like "Column Family description"    
+    
+    it "should have autoload set to true" do
+      @subject.autoload.should be_true
+    end
+    
   end
   
 end
