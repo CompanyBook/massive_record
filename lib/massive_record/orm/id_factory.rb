b081d387a806a859b7bcdf92be0bf3eae93e270c
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-21 11:10:40 +0100

    Started to describe the public interface for the factory.

--- lib/massive_record/orm/id_factory.rb
+++ lib/massive_record/orm/id_factory.rb
@@ -4,6 +4,19 @@ module MassiveRecord
   module ORM
     class IdFactory < Table
       include Singleton
+
+      def next_for(table)
+        table = table.respond_to?(:table_name) ? table.table_name : table.to_s
+        next_id :table => table
+      end
+
+
+      private
+
+
+      def next_id(options = {})
+
+      end
     end
   end
 end
--- spec/orm/cases/id_factory_spec.rb
+++ spec/orm/cases/id_factory_spec.rb
@@ -1,7 +1,52 @@
 require 'spec_helper'
+require 'orm/models/person'
 
 describe "id factory" do
   it "should be a singleton" do
     MassiveRecord::ORM::IdFactory.included_modules.should include(Singleton)
+  end
+
+  it "should return an instance of self" do
+    MassiveRecord::ORM::IdFactory.instance.should be_instance_of MassiveRecord::ORM::IdFactory
+  end
+
+  describe "#next_for" do
+    describe "dry" do
+      include MockMassiveRecordConnection
+
+      before do
+        @factory = MassiveRecord::ORM::IdFactory.instance
+      end
+
+      it "should respond to next_for" do
+        @factory.should respond_to :next_for
+      end
+
+      it "should use incomming table name if it's a string" do
+        @factory.should_receive(:next_id).with(hash_including(:table => "test_table"))
+        @factory.next_for "test_table"
+      end
+
+      it "should use incomming table name if it's a symbol" do
+        @factory.should_receive(:next_id).with(hash_including(:table => "test_table"))
+        @factory.next_for :test_table
+      end
+
+      it "should ask object for it's table name if it responds to that" do
+        Person.should_receive(:table_name).and_return("people")
+        @factory.should_receive(:next_id).with(hash_including(:table => "people"))
+        @factory.next_for(Person)
+      end
+    end
+
+
+
+    describe "database" do
+      
+
+      before do
+        @factory = MassiveRecord::ORM::IdFactory.instance
+      end
+    end
   end
 end
