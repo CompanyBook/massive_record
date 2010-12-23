f4badb5c8f6213ea3a916d0dacfbdc52052a88cc
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-23 15:42:15 +0100

    Column Families inject self into a family when added to a set.

--- lib/massive_record/orm/schema/column_families.rb
+++ lib/massive_record/orm/schema/column_families.rb
@@ -4,7 +4,11 @@ module MassiveRecord
   module ORM
     module Schema
       class ColumnFamilies < Set
-
+        def add(family)
+          super
+          family.column_families = self
+        end
+        alias_method :<<, :add
       end
     end
   end
--- spec/orm/schema/column_families_spec.rb
+++ spec/orm/schema/column_families_spec.rb
@@ -10,15 +10,27 @@ describe MassiveRecord::ORM::Schema::ColumnFamilies do
   end
 
   it "should be possible to add column families" do
-    family = MassiveRecord::ORM::Schema::ColumnFamily.new(:info)
+    family = MassiveRecord::ORM::Schema::ColumnFamily.new(:name => :info)
     @column_families << family
     @column_families.first.should == family
   end
 
   it "should not be possible to add two column families with the same name" do
-    family_1 = MassiveRecord::ORM::Schema::ColumnFamily.new(:info)
-    family_2 = MassiveRecord::ORM::Schema::ColumnFamily.new(:info)
+    family_1 = MassiveRecord::ORM::Schema::ColumnFamily.new(:name => :info)
+    family_2 = MassiveRecord::ORM::Schema::ColumnFamily.new(:name => :info)
     @column_families << family_1
     @column_families.add?(family_2).should be_nil
+  end
+
+  it "should add self to column_family when familiy is added" do
+    family = MassiveRecord::ORM::Schema::ColumnFamily.new(:name => :info)
+    @column_families << family
+    family.column_families.should == @column_families
+  end
+
+  it "should add self to column_family when familiy is added with a question" do
+    family = MassiveRecord::ORM::Schema::ColumnFamily.new(:name => :info)
+    @column_families.add? family
+    family.column_families.should == @column_families
   end
 end
