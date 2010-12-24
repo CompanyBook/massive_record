f297638f0263c0c51704394c1add24ab38362cdb
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-24 11:41:51 +0100

    Made field comparable to other fields

--- lib/massive_record/orm/schema/field.rb
+++ lib/massive_record/orm/schema/field.rb
@@ -13,6 +13,21 @@ module MassiveRecord
           self.type = options[:type]
           self.default = options[:default]
         end
+
+        def ==(other)
+          other.instance_of?(self.class) && other.hash == hash
+        end
+        alias_method :eql?, :==
+
+        def hash
+          name.hash
+        end
+
+        private
+
+        def name=(name)
+          @name = name.to_s
+        end
       end
     end
   end
--- spec/orm/schema/field_spec.rb
+++ spec/orm/schema/field_spec.rb
@@ -9,4 +9,24 @@ describe MassiveRecord::ORM::Schema::Field do
       end
     end
   end
+
+  it "should cast name to string" do
+    field = MassiveRecord::ORM::Schema::Field.new(:name => :name)
+    field.name.should == "name"
+  end
+
+  it "should compare two column families based on name" do
+    field_1 = MassiveRecord::ORM::Schema::Field.new(:name => :name)
+    field_2 = MassiveRecord::ORM::Schema::Field.new(:name => :name)
+
+    field_1.should == field_2
+    field_1.eql?(field_2).should be_true
+  end
+
+  it "should have the same hash value for two families with the same name" do
+    field_1 = MassiveRecord::ORM::Schema::Field.new(:name => :name)
+    field_2 = MassiveRecord::ORM::Schema::Field.new(:name => :name)
+
+    field_1.hash.should == field_2.hash
+  end
 end
