a5f9195045dddbf3e679c464c68f69f26e1f69df
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-24 11:59:03 +0100

    Fields set does now inject self in field when fields are added.

--- lib/massive_record/orm/schema/fields.rb
+++ lib/massive_record/orm/schema/fields.rb
@@ -1,8 +1,15 @@
 module MassiveRecord
   module ORM
     module Schema
-      class Fields < Set
+      class InvalidField < ArgumentError; end
 
+      class Fields < Set
+        def add(field)
+          field.fields = self
+          raise InvalidField.new(field.errors.full_messages.join(". ")) unless field.valid?
+          super
+        end
+        alias_method :<<, :add
       end
     end
   end
--- spec/orm/schema/fields_spec.rb
+++ spec/orm/schema/fields_spec.rb
@@ -9,12 +9,25 @@ describe MassiveRecord::ORM::Schema::Fields do
     @fields.should be_a_kind_of Set
   end
 
-  it "should be possible to add fields" do
-    @fields << MassiveRecord::ORM::Schema::Field.new
-  end
+  describe "add fields to the set" do
+    it "should be possible to add fields" do
+      @fields << MassiveRecord::ORM::Schema::Field.new(:name => "field")
+    end
 
-  it "should not be possible to add two fields with the same name" do
-    @fields << MassiveRecord::ORM::Schema::Field.new(:name => "attr")
-    @fields.add?(MassiveRecord::ORM::Schema::Field.new(:name => "attr")).should be_nil
+    it "should add self to field's fields attribute" do
+      field = MassiveRecord::ORM::Schema::Field.new(:name => :field)
+      @fields << field
+      field.fields.should == @fields
+    end
+
+    it "should not be possible to add two fields with the same name" do
+      @fields << MassiveRecord::ORM::Schema::Field.new(:name => "attr")
+      @fields.add?(MassiveRecord::ORM::Schema::Field.new(:name => "attr")).should be_nil
+    end
+
+    it "should raise error if invalid column familiy is added" do
+      invalid_field = MassiveRecord::ORM::Schema::Field.new
+      lambda { @fields << invalid_field }.should raise_error MassiveRecord::ORM::Schema::InvalidField
+    end
   end
 end
