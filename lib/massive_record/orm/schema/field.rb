c895f5d7294d0f27ac1225800827e2a99b900448
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-24 10:43:45 +0100

    Implemented a stupid field class. Will fix it later.

--- lib/massive_record/orm/schema/field.rb
+++ lib/massive_record/orm/schema/field.rb
@@ -2,7 +2,17 @@ module MassiveRecord
   module ORM
     module Schema
       class Field
-        # :name, :column_family, :column, :type, :default
+        attr_accessor :name, :column_family, :column, :type, :default
+
+        def initialize(*args)
+          options = args.extract_options!.to_options
+
+          self.name = options[:name]
+          self.column = options[:column]
+          self.column_family = options[:column_family]
+          self.type = options[:type]
+          self.default = options[:default]
+        end
       end
     end
   end
--- spec/orm/schema/field_spec.rb
+++ spec/orm/schema/field_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe MassiveRecord::ORM::Schema::Field do
-  
+  describe "initializer" do
+    %w(name column_family column type default).each do |attr_name|
+      it "should set #{attr_name}" do
+        field = MassiveRecord::ORM::Schema::Field.new attr_name => "a_value"
+        field.send(attr_name).should == "a_value"
+      end
+    end
+  end
 end
