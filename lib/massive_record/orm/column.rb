de818ec620f8e808e5618a5bebd6028f8cea8617
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-12-27 13:17:50 +0100

    Undefined a lot of method in Column which is related to persistence which we do not currently support.
    I guess we might support some of them in time, when our associations are
    working, but right now it is easier to just make sure that objects does
    not respond to them.

--- lib/massive_record/orm/column.rb
+++ lib/massive_record/orm/column.rb
@@ -4,6 +4,16 @@ module MassiveRecord
   module ORM
     class Column < Base
       include Schema::ColumnInterface
+
+      # TODO  Column does not support these kind of methods
+      class << self
+        undef_method :first, :last, :all, :exists?, :destroy_all
+      end
+
+      undef_method :create, :new_record?, :persisted?, :destroyed?,
+        :reload, :save, :save!, :update_attribute, :update_attributes,
+        :update_attributes!, :touch, :destroy, :increment!, :atomic_increment!,
+        :decrement!, :delete
     end
   end
 end
--- spec/orm/cases/column_spec.rb
+++ spec/orm/cases/column_spec.rb
@@ -24,4 +24,26 @@ describe "column classes" do
   it "should have a default value set" do
     @address.nice_place.should be_true
   end
+
+
+  # TODO  We might want to remove this when we have implemented
+  #       associations correctly. Since Columns are contained within
+  #       tables, calling save should do something on it's owner object.
+  describe "not be possible to persist (at least for now...)" do
+    %w(first last all exists? destroy_all).each do |method|
+      it "should not respond to class method #{method}" do
+        Address.should_not respond_to method
+      end
+    end
+
+    %w(
+      create! create new_record? persisted? destroyed? reload save save!
+      update_attribute update_attributes update_attributes! touch destroy
+      delete increment! atomic_increment! decrement!
+    ).each do |method|
+      it "should not respond to instance method #{method}" do
+        @address.should_not respond_to method
+      end
+    end
+  end
 end
