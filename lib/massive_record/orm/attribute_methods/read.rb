CompanyBook/massive_record

commit 327a16badf7d10a4b62a6ee44c4a75cae36c5a1d

Add basic casting on reading ORM values

lib/massive_record/orm/attribute_methods/read.rb
@@ -9,7 +9,7 @@ module MassiveRecord
         end
         
         def read_attribute(attr_name)
-          @attributes[attr_name.to_s]
+          attributes_schema[attr_name].nil? ? @attributes[attr_name.to_s] : attributes_schema[attr_name].decode(@attributes[attr_name.to_s])
         end
 
         private

lib/massive_record/orm/field.rb
@@ -22,9 +22,9 @@ module MassiveRecord
         when :string
           value
         when :boolean
-          value.empty? ? nil : !value.match(/^(true|1)$/i).nil?
+          value.to_s.empty? ? nil : !value.to_s.match(/^(true|1)$/i).nil?
         when :integer
-          value.empty? ? nil : value.to_i
+          value.to_s.empty? ? nil : value.to_i
         when :date
           value.empty? ? nil : Date.parse(value)
         when :time

spec/orm/cases/columns_spec.rb
@@ -6,6 +6,9 @@ describe "Person" do
   
   before do
     @person = Person.new
+    @person.points = "25"
+    @person.date_of_birth = "19850730"
+    @person.status = "0"
   end
   
   it "should have a list of column families" do
@@ -22,16 +25,14 @@ describe "Person" do
   end
   
   it "should default an attribute to its default value" do
-    @person.points.should == 1
+    @person.points.should == 25
   end
   
   it "should parse a Date field properly" do
-    pending "Casting not implemented, yet"
-    @person.date_of_birth.should be_kind_of_a(Date)
+    @person.date_of_birth.should be_a_kind_of(Date)
   end
   
   it "should parse a Boolean field properly" do
-    pending "Casting not implemented, yet"
     @person.status.should be_false
   end
   

spec/orm/cases/finders_spec.rb
@@ -119,7 +119,7 @@ describe "finders" do
     it "should return the person object when found" do
       @person.name.should == "John Doe"
       @person.email.should == "john@base.com"
-      @person.age.should == "20"
+      @person.age.should == 20
     end
 
     it "should find first person" do

spec/orm/cases/table_spec.rb
@@ -26,7 +26,6 @@ describe "table" do
     end
     
     it "should have one autoloaded column family" do
-      p @subject.autoloaded_column_family_names
       @subject.autoloaded_column_family_names.should be_a_kind_of(Array)
       @subject.autoloaded_column_family_names.size.should == 1
       @subject.autoloaded_column_family_names.first.should == :sandbox

spec/orm/table_attributes_spec.rb
@@ -1,73 +1,0 @@
-require 'spec_helper'
-
-describe MassiveRecord::ORM::Table do
-  
-  before do
-    @connection ||= MassiveRecord::Wrapper::Connection.new(:host => MR_CONFIG['host'], :port => MR_CONFIG['port'])
-  end
-  
-  describe "and a new Person instance" do
-    
-    before do
-      @person = Person.new
-    end
-    
-    it "should fail to save without any attributes" do
-      @person.save.should be_false
-    end
-        
-    it "should return a nil ID" do
-      @person.id.should be_nil
-    end
-    
-    it "should a list of the available attributes" do
-      @person.attributes.should be_instance_of Hash
-      @person.attributes.keys.should include *%w(name email date_of_birth status)
-    end
-    
-    it "should set a value" do
-      @person.name = "John"
-      @person.name.should == "John"
-    end
-    
-    it "should mass assign values" do
-      @person.attributes = {:name => "Foo", :age => 55}
-      @person.name.should == "Foo"
-      @person.age.should == 55
-    end
-  end
-  
-  describe "and an existing Person instance" do
-    
-    before do
-      @person = Person.new({
-        :first_name => "John",
-        :last_name => "Doe",
-        :email => "john@doe.com",
-        :date_of_birth => "Fri, 30 Nov 1990",
-        :status => "1"
-      })
-      
-      @person.id = "ID1"
-    end
-    
-    it "should have an ID" do
-      @person.id.should == "ID1"
-    end
-    
-    it "should have 5 attributes" do
-      @person.attributes.size == 5
-    end
-    
-    it "should parse Date format" do
-      pending "casting is not in place, yet."
-      @person.date_of_birth.should be_instance_of Date
-    end
-    
-    it "should parse Boolean format" do
-      @person.status.should be_true
-    end
-    
-  end
-    
-end

commit 41ee41f65e2f7362debc1a55719bb1a3fb0203da

Bugfix: read_attribute can now again support symbols.
Error was introduced in 327a16ba, and error was that attr_name was not
casted to string, which is must be as @attributes has string keys.

lib/massive_record/orm/attribute_methods/read.rb
@@ -9,7 +9,8 @@ module MassiveRecord
         end
         
         def read_attribute(attr_name)
-          attributes_schema[attr_name].nil? ? @attributes[attr_name.to_s] : attributes_schema[attr_name].decode(@attributes[attr_name.to_s])
+          attr_name = attr_name.to_s
+          attributes_schema[attr_name].nil? ? @attributes[attr_name] : attributes_schema[attr_name].decode(@attributes[attr_name])
         end
 
         private

spec/orm/cases/attribute_methods_spec.rb
@@ -3,7 +3,7 @@ require 'orm/models/person'
 
 describe "attribute methods" do
   before do
-    @model = Person.new :id => 5, :name => "John", :age => 15
+    @model = Person.new :id => 5, :name => "John", :age => "15"
   end
 
   it "should define reader method" do
@@ -22,6 +22,10 @@ describe "attribute methods" do
 
   it "should be possible to read attributes" do
     @model.read_attribute(:name).should == "John"
+  end
+
+  it "should return casted value when read" do
+    @model.read_attribute(:age).should == 15
   end
   
   it "should contains the id in the attributes getter" do