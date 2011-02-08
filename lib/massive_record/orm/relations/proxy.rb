55bb37eb93dcd8d6f643ab1909872fa7dd0e5537
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-02-08 14:04:37 +0100

    Proxy now knows if it has loaded target object, it can be reset and it forwards method correctly.

--- lib/massive_record/orm/relations/proxy.rb
+++ lib/massive_record/orm/relations/proxy.rb
@@ -5,13 +5,33 @@ module MassiveRecord
     # Parent class for all proxies sitting between records
     #
     class Proxy
-      instance_methods.each { |m| undef_method m unless m.to_s =~ /^(?:nil\?|send|object_id|to_a|inspect|to_s)$|^__|^respond_to|^should/ }
+      instance_methods.each { |m| undef_method m unless m.to_s =~ /^(?:nil\?|send|object_id|to_a|inspect|to_s|extend|)$|^__|^respond_to|^should|^instance_variable_/ }
 
-      attr_accessor :owner, :target, :metadata, :loaded
+      attr_accessor :owner, :target, :metadata
 
 
       def loaded?
         !!@loaded
+      end
+
+      def loaded!
+        @loaded = true
+      end
+
+      def reset
+        @loaded = @target = nil
+      end
+
+
+      def respond_to?(*args)
+        super || (target && target.respond_to?(*args))
+      end
+
+      def method_missing(method, *args, &block)
+        return target.send(method, *args, &block) if target && target.respond_to?(method)
+        super
+      rescue NoMethodError => e
+        raise e, e.message.sub(/ for #<.*$/, " via proxy for #{target}")
       end
     end
   end
--- spec/shared/orm/relations/proxy.rb
+++ spec/shared/orm/relations/proxy.rb
@@ -1,19 +1,81 @@
 shared_examples_for MassiveRecord::ORM::Proxy do
-  %w(owner target metadata loaded).each do |method|
+  %w(owner target metadata).each do |method|
     it "should respond to #{method}" do
       should respond_to method
     end
   end
 
-  describe "#loaded?" do
+  describe "#loaded" do
     it "should be true when loaded" do
-      subject.loaded = true
+      subject.instance_variable_set :@loaded, true
       should be_loaded
     end
 
     it "should be false when not loaded" do
-      subject.loaded = false
+      subject.instance_variable_set :@loaded, false
       should_not be_loaded
+    end
+
+    it "should be loaded when loaded! is called" do
+      subject.instance_variable_set :@loaded, false
+      subject.loaded!
+      should be_loaded
+    end
+  end
+
+
+  describe "#reset" do
+    it "should not be loaded after reset" do
+      subject.loaded!
+      subject.reset
+      should_not be_loaded
+    end
+
+    it "should reset the target" do
+      subject.target = "foo"
+      subject.reset
+      subject.target.should be_nil
+    end
+  end
+
+
+  describe "forward method calls to target" do
+    let(:target) { mock(Object, :target_method => "return value") }
+
+    before do
+      subject.target = target
+    end
+    
+
+    describe "#respond_to?" do
+      it "should check proxy to see if it responds to something" do
+        should respond_to :target
+      end
+      
+      it "should respond to target_method" do
+        should respond_to :target_method
+      end
+
+      it "should not respond to a dummy method" do
+        should_not respond_to :dummy_method_which_does_not_exists 
+      end
+    end
+
+
+    describe "#method_missing" do
+      it "should call proxy's method if exists in proxy" do
+        subject.should_receive(:loaded?).once
+        subject.loaded?
+      end
+
+      it "should call target's method if it responds to it" do
+        target.should_receive(:target_method).and_return("foo")
+        subject.target_method.should == "foo"
+      end
+
+      it "should rause no method error if no one responds to it" do
+        lambda { subject.dummy_method_which_does_not_exists }.should raise_error NoMethodError
+      end
     end
   end
 end
