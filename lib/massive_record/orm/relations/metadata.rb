1c75c5797152d568182cc0d657e51c414011ee3d
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-02-08 15:56:44 +0100

    Started working on the meta data object for relations.
    This object will contain information like relation's foreign keys, class
    name, etc.

---
+++ lib/massive_record/orm/relations/metadata.rb
@@ -0,0 +1,32 @@
+module MassiveRecord
+  module ORM
+    module Relations
+
+      #
+      # The master of metadata related to a relation. For instance;
+      # references_one :employee, :foreign_key => "person_id", :class_name => "Person"
+      #
+      class Metadata
+        attr_accessor :name
+        attr_writer :foreign_key, :class_name
+
+        def foreign_key
+          @foreign_key
+        end
+
+        def class_name
+          (@class_name || calculate_class_name).to_s
+        end
+
+
+
+        private
+
+
+        def calculate_class_name
+          name.to_s.classify
+        end
+      end
+    end
+  end
+end
---
+++ spec/orm/relations/metadata_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'orm/models/person'
+
+describe MassiveRecord::ORM::Relations::Metadata do
+  subject { MassiveRecord::ORM::Relations::Metadata.new }
+
+  %w(name foreign_key class_name).each do |attr|
+    it { should respond_to attr }
+    it { should respond_to attr+"=" }
+  end
+
+
+  describe "#class_name" do
+    it "should return whatever it's being set to" do
+      subject.class_name = "Person"
+      subject.class_name.should == "Person"
+    end
+
+    it "should return class name as a string" do
+      subject.class_name = Person
+      subject.class_name.should == "Person"
+    end
+
+    it "should calculate it from name" do
+      subject.name = :employee
+      subject.class_name.should == "Employee"
+    end
+  end
+
+
+
+
+  describe "#foreign_key" do
+    it "should return whatever it's being set to" do
+      subject.foreign_key = "person_id"
+      subject.foreign_key.should == "person_id"
+    end
+
+    it "should try and calculate the foreign key from the class name" do
+      pending
+    end
+  end
+end
