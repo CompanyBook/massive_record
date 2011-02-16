51e710b7f6505af2c0299d5641746057ee277ce8
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-02-16 10:15:10 +0100

    Adding records to references many relations now starts to work.

--- lib/massive_record/orm/relations/proxy/references_many.rb
+++ lib/massive_record/orm/relations/proxy/references_many.rb
@@ -9,6 +9,33 @@ module MassiveRecord
             @target = []
           end
 
+
+          #
+          # Adding record(s) to the collection.
+          #
+          def <<(*records)
+            records.flatten.each do |record|
+              unless include? record
+                raise_if_type_mismatch(record)
+                add_foreign_key_in_owner(record.id)
+                target << record
+              end
+            end
+
+            self
+          end
+          alias_method :push, :<<
+          alias_method :concat, :<<
+          #
+          # Checks if record is included in collection
+          #
+          # TODO  This needs a bit of work, depending on if proxy's target
+          #       has been loaded or not. For now, we are just checking
+          #       what we currently have in @target
+          #
+          def include?(record)
+            target.include? record
+          end
           private
 
 
@@ -22,6 +49,16 @@ module MassiveRecord
 
           def can_find_target?
             super || owner.send(foreign_key).any? 
+          end
+
+
+          def add_foreign_key_in_owner(id)
+            will_change_notification_method = foreign_key+"_will_change!"
+
+            if owner.respond_to? foreign_key_setter
+              owner.send(foreign_key) << id
+              owner.send(will_change_notification_method) if owner.respond_to? will_change_notification_method
+            end
           end
         end
       end
--- spec/orm/relations/proxy/references_many_spec.rb
+++ spec/orm/relations/proxy/references_many_spec.rb
@@ -6,8 +6,9 @@ describe TestReferencesManyProxy do
   include SetUpHbaseConnectionBeforeAll 
   include SetTableNamesToTestTable
 
-  let(:owner) { Person.new }
+  let(:owner) { Person.new :id => "person-id-1", :name => "Test", :age => 29 }
   let(:target) { TestClass.new :id => "test-class-id-1" }
+  let(:target_2) { TestClass.new :id => "test-class-id-2" }
   let(:metadata) { subject.metadata }
 
   subject { owner.send(:relation_proxy, 'test_classes') }
@@ -51,6 +52,57 @@ describe TestReferencesManyProxy do
     it "should always wrap the proc's result in an array" do
       TestClass.should_receive(:find).with("testing-123").and_return(test_class)
       subject.load_target.should == [test_class]
+    end
+  end
+
+
+  describe "adding records to collection" do
+    [:<<, :push, :concat].each do |add_method|
+      describe "by ##{add_method}" do
+        it "should include the target in the proxy" do
+          subject.send(add_method, target)
+          subject.target.should include target
+        end
+
+        it "should update array of foreign keys in owner" do
+          owner.test_classes_ids.should be_empty
+          subject.send(add_method, target)
+          owner.test_classes_ids.should include(target.id)
+        end
+
+        it "should update array of foreign keys in owner" do
+          owner.save!
+          subject.send(add_method, target)
+          owner.save! and owner.reload
+          owner.test_classes_ids.should include(target.id)
+        end
+
+        it "should not update array of foreign keys in owner if it does not respond to it" do
+          owner.should_receive(:respond_to?).and_return(false)
+          subject.send(add_method, target)
+          owner.test_classes_ids.should_not include(target.id)
+        end
+
+        it "should not do anything adding the same record twice" do
+          2.times { subject.send(add_method, target) }
+          subject.target.length.should == 1
+          owner.test_classes_ids.length.should == 1
+        end
+
+        it "should be able to add two records at the same time" do
+          subject.send add_method, [target, target_2]
+          subject.target.should include target
+          subject.target.should include target_2
+        end
+
+        it "should return proxy so calls can be chained" do
+          subject.send(add_method, target).should == subject
+        end
+
+        it "should raise an error if there is a type mismatch" do
+          lambda { subject.send add_method, Person.new }.should raise_error MassiveRecord::ORM::RelationTypeMismatch
+        end
+      end
     end
   end
 end
