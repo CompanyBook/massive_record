bc2e6c89271f9700f5d170016d43b3e9bbe42378
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-04-05 13:02:41 +0200

    Kinda restored old behaviour:
    We are now serializing and deserializing all
    values going back and forth to hbase. This is
    close to what happened before, except that we'll
    do this even smarter, giving the responsibility
    of serialize and deserialize to the Schema::Field.

--- lib/massive_record/orm/persistence.rb
+++ lib/massive_record/orm/persistence.rb
@@ -91,7 +91,7 @@ module MassiveRecord
         attr_name = attr_name.to_s
 
         row = row_for_record
-        row.values = attributes_to_row_values_hash([attr_name])
+        row.values = attributes_to_row_values_hash([attr_name], true)
         self[attr_name] = row.atomic_increment(attributes_schema[attr_name].unique_name, by).to_i
       end
 
@@ -139,7 +139,7 @@ module MassiveRecord
       #
       def store_record_to_database(attribute_names_to_update = [])
         row = row_for_record
-        row.values = attributes_to_row_values_hash(attribute_names_to_update)
+        row.values = attributes_to_row_values_hash(attribute_names_to_update, true)
         row.save
       end
 
@@ -185,12 +185,15 @@ module MassiveRecord
       #
       # Returns attributes on a form which Wrapper::Row expects
       #
-      def attributes_to_row_values_hash(only_attr_names = [])
+      def attributes_to_row_values_hash(only_attr_names = [], encode = false)
         values = Hash.new { |hash, key| hash[key] = Hash.new }
 
         attributes_schema.each do |attr_name, orm_field|
           next unless only_attr_names.empty? || only_attr_names.include?(attr_name)
-          values[orm_field.column_family.name][orm_field.column] = send(attr_name)
+          value = send(attr_name)
+          value = coder.dump(value) if encode
+
+          values[orm_field.column_family.name][orm_field.column] = value
         end
 
         values
