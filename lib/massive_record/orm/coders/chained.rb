94a7c2f5e61ff0dc3e14b74afa2365896bfc158d
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-04-06 15:35:56 +0200

    We can now chain coders, make it try the next in the chain if one fails.
    This will give us support for for instance reading old YAML data in the
    database, while still persiting new data with JSON.

--- lib/massive_record/orm/coders/chained.rb
+++ lib/massive_record/orm/coders/chained.rb
@@ -1,11 +1,22 @@
 module MassiveRecord
   module ORM
     module Coders
+      CoderError = ::StandardError
+      ParseError = CoderError
+      EncodeError = CoderError
 
       #
       # If you ever need support for multiple coders, this class can help you out.
       # Lets say you have YAML serialized data in your attributes, but what to migrate
       # over to JSON, you can:
+      #   
+      #   MassiveRecord::ORM::Coders::Chained.new(
+      #     MassiveRecord::ORM::Coders::JSON.new,
+      #     MassiveRecord::ORM::Coders::YAML.new
+      #   )
+      #
+      #   or
+      #
       #   MassiveRecord::ORM::Base.coder = MassiveRecord::ORM::Coders::Chained.new({
       #     :load_with => [MassiveRecord::ORM::Coders::JSON.new, MassiveRecord::ORM::Coders::JSON.new],
       #     :dump_with => MassiveRecord::ORM::Coders::JSON.new
@@ -20,27 +31,39 @@ module MassiveRecord
         def initialize(*args)
           coders = args.extract_options!
 
-          @loaders = []
-          @dumpers = []
+          @loaders = args.flatten
+          @dumpers = args.flatten
 
-          @loaders |= [coders[:load_with]].flatten if coders[:load_with]
-          @dumpers |= [coders[:dump_with]].flatten if coders[:dump_with]
+          @loaders = [coders[:load_with]].flatten if coders[:load_with]
+          @dumpers = [coders[:dump_with]].flatten if coders[:dump_with]
         end
+
 
         def dump(object)
           raise "We have no coders to dump with" if dumpers.empty?
 
           dumpers.each do |coder|
-            return coder.dump(object)
+            begin
+              return coder.dump(object)
+            rescue
+            end
           end
+
+          raise "Unable to encode #{object}. Tried encode it with: #{dumpers.collect(&:class).to_sentence}"
         end
+
 
         def load(data)
           raise "We have no coders to load with" if loaders.empty?
 
           loaders.each do |coder|
-            return coder.load(data)
+            begin
+              return coder.load(data)
+            rescue
+            end
           end
+
+          raise "Unable to parse #{data}. Tried loading it with: #{loaders.collect(&:class).to_sentence}"
         end
       end
     end
