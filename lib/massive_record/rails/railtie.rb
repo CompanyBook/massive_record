f5f49509831b20c01febc47e401ccd415d7712b1
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-05-18 11:59:43 +0200

    Added statistics at the end of a query how much time spent in MR.
    Completed 200 OK in 1646ms (Views: 1519.6ms | MassiveRecord: 4.5ms)
    Hooked MassiveRecord into ActionController's 
    instrumentation class to get this functionality.

--- lib/massive_record/rails/railtie.rb
+++ lib/massive_record/rails/railtie.rb
@@ -5,9 +5,18 @@ module MassiveRecord
         MassiveRecord::ORM::Base.logger = ::Rails.logger
       end
 
+      # Make Rails handle RecordNotFound correctly in production
       initializer "massive_record.action_dispatch" do
         ActiveSupport.on_load :action_controller do
           ActionDispatch::ShowExceptions.rescue_responses.update('MassiveRecord::ORM::RecordNotFound' => :not_found)
+        end
+      end
+
+      # Expose database runtime to controller for logging.
+      initializer "massive_record.log_runtime" do
+        require "massive_record/rails/controller_runtime"
+        ActiveSupport.on_load(:action_controller) do |app|
+          include MassiveRecord::Rails::ControllerRuntime
         end
       end
     end
