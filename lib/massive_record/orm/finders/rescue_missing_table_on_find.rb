c0e6f845d081e526f4183efbac6cbb32eaa6771b
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-05-24 13:37:41 +0200

    Rescue error raised when scanner is used on non-existing table.

--- lib/massive_record/orm/finders/rescue_missing_table_on_find.rb
+++ lib/massive_record/orm/finders/rescue_missing_table_on_find.rb
@@ -1,5 +1,10 @@
 module MassiveRecord
   module ORM
+    #
+    # Module which adds functionality so we rescue errors which might occur on
+    # find calls when we are querying tables which does not exist.
+    # Small problem with this, which will need to look into.
+    #
     module RescueMissingTableOnFind
       def do_find(*args)
         create_table_and_retry_if_table_missing { super }
@@ -14,10 +19,18 @@ module MassiveRecord
       private
 
 
-      def create_table_and_retry_if_table_missing
+      #
+      # Yields the block and if any errors occur we will check if table does exist or not.
+      # Create it if it's missing and try again.
+      #
+      # Errors which we'll retry on are:
+      #   Apache::Hadoop::Hbase::Thrift::IOError          -> Raised on simple find(id) calls
+      #   Apache::Hadoop::Hbase::Thrift::IllegalArgument  -> Raised when a scanner is used
+      #
+      def create_table_and_retry_if_table_missing # :nodoc:
         begin
           yield
-        rescue Apache::Hadoop::Hbase::Thrift::IOError => error
+        rescue Apache::Hadoop::Hbase::Thrift::IOError, Apache::Hadoop::Hbase::Thrift::IllegalArgument => error
           if table.exists?
             raise error
           else
