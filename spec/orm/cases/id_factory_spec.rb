64266cdfbb8ffef9d230e307c8a4a648e9968530
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-06-08 09:31:58 +0200

    Just rewrote id_factory_spec to use subject.

--- spec/orm/cases/id_factory_spec.rb
+++ spec/orm/cases/id_factory_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'orm/models/person'
 
 describe "id factory" do
+  subject { MassiveRecord::ORM::IdFactory.instance }
+
   it "should be a singleton" do
     MassiveRecord::ORM::IdFactory.included_modules.should include(Singleton)
   end
@@ -10,32 +12,28 @@ describe "id factory" do
     describe "dry" do
       include MockMassiveRecordConnection
 
-      before do
-        @factory = MassiveRecord::ORM::IdFactory.instance
-      end
-
       it "should respond to next_for" do
-        @factory.should respond_to :next_for
+        subject.should respond_to :next_for
       end
 
       it "should use incomming table name if it's a string" do
-        @factory.should_receive(:next_id).with(hash_including(:table => "test_table"))
-        @factory.next_for "test_table"
+        subject.should_receive(:next_id).with(hash_including(:table => "test_table"))
+        subject.next_for "test_table"
       end
 
       it "should use incomming table name if it's a symbol" do
-        @factory.should_receive(:next_id).with(hash_including(:table => "test_table"))
-        @factory.next_for :test_table
+        subject.should_receive(:next_id).with(hash_including(:table => "test_table"))
+        subject.next_for :test_table
       end
 
       it "should ask object for it's table name if it responds to that" do
         Person.should_receive(:table_name).and_return("people")
-        @factory.should_receive(:next_id).with(hash_including(:table => "people"))
-        @factory.next_for(Person)
+        subject.should_receive(:next_id).with(hash_including(:table => "people"))
+        subject.next_for(Person)
       end
 
       it "should have class method next_for and delegate it to it's instance" do
-        @factory.should_receive(:next_for).with("cars")
+        subject.should_receive(:next_for).with("cars")
         MassiveRecord::ORM::IdFactory.next_for("cars")
       end
     end
@@ -46,29 +44,25 @@ describe "id factory" do
       include SetUpHbaseConnectionBeforeAll
       include SetTableNamesToTestTable
 
-      before do
-        @factory = MassiveRecord::ORM::IdFactory.instance
-      end
-
       after do
         MassiveRecord::ORM::IdFactory.destroy_all
         MassiveRecord::ORM::IdFactory.instance_variable_set(:@instance, nil)
       end
 
       it "should increment start a new sequence on 1" do
-        @factory.next_for(Person).should == 1
+        subject.next_for(Person).should == 1
       end
 
       it "should increment value one by one" do
         5.times do |index|
           expected_id = index + 1
-          @factory.next_for(Person).should == expected_id
+          subject.next_for(Person).should == expected_id
         end
       end
 
       it "should maintain ids separate for each table" do
-        3.times { @factory.next_for(Person) }
-        @factory.next_for("cars").should == 1
+        3.times { subject.next_for(Person) }
+        subject.next_for("cars").should == 1
       end
     end
   end
