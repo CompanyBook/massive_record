f002af40e72da983dd669cbcd10d496c74eea967
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-07-20 10:38:14 +0200

    Wrote really short documentation on how to use observer.
    People should feel at home if they have ever used ActiveRecord's
    observers.

--- lib/massive_record/orm/observer.rb
+++ lib/massive_record/orm/observer.rb
@@ -1,5 +1,41 @@
 module MassiveRecord
   module ORM
+
+    #
+    # MassiveRecord Observer. Greatly influenced by ActiveRecord's
+    # way of doing callbacks, thus should feel familiar to most people.
+    #
+    # NOTE  that if you are using rails you should add observers to into
+    #       your application.rb configuration file like this:
+    #
+    #       config.massive_record.observers = :person_observer, :audit_observer
+    #
+    #       This will ensure that observers are loaded correctly. If you are not
+    #       using rails you can do: MassiveRecord::ORM::Base.instantiate_observers
+    #       after your application has been initialized.
+    #
+    # Example of usage:
+    #
+    # class Person < MassiveRecord::ORM::Table
+    #   column_family :info do
+    #     field :name
+    #   end
+    # end
+    #
+    # class PersonObserver < MassiveRecord::ORM::Observer
+    #   def after_save(saved_person_record)
+    #     # do something here after people are being saved
+    #   end
+    # end
+    #
+    # class AuditObserver < MassiveRecord::ORM::Observer
+    #   observe :person, :and, :other, :classes
+    #
+    #   def after_save(saved_person_record)
+    #     # do something here after people are being saved
+    #   end
+    # end
+    #
     class Observer < ActiveModel::Observer
       protected
 
