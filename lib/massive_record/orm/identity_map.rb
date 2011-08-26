927fbd5a802ffbc0f7653c2c4be54d3d892b081e
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-08-26 11:22:34 +0200

    Added repository, add, get and remove on IdentityMap.

--- lib/massive_record/orm/identity_map.rb
+++ lib/massive_record/orm/identity_map.rb
@@ -1,5 +1,15 @@
 module MassiveRecord
   module ORM
+
+    #
+    # The goal of the IdentiyMap is to make sure that the same object is not loaded twice
+    # from the database, but uses the same object if you do 2.times { AClass.find(1) }.
+    #
+    # To get a quick introduction on IdentityMap see: http://www.martinfowler.com/eaaCatalog/identityMap.html
+    #
+    # You can enable / disable Identity map by doing:
+    # MassiveRecord::ORM::IdentityMap.enabled = flag
+    #
     class IdentityMap
       class << self
         def enabled=(boolean)
@@ -10,6 +20,37 @@ module MassiveRecord
           Thread.current[:identity_map_enabled]
         end
         alias enabled? enabled
+
+
+        def repository
+          Thread.current[:identity_map_repository] ||= Hash.new { |hash, key| hash[key] = {} }
+        end
+        delegate :clear, :to => :repository
+
+
+        def get(klass, id)
+          repository[class_to_repository_key(klass)][id]
+        end
+
+        def add(record)
+          repository[record_class_to_repository_key(record)][record.id] = record
+        end
+
+        def remove(record)
+          repository[record_class_to_repository_key(record)].delete record.id
+        end
+
+
+
+        private
+
+        def record_class_to_repository_key(record)
+          class_to_repository_key record.class
+        end
+
+        def class_to_repository_key(klass)
+          klass.to_s
+        end
       end
     end
   end
--- spec/orm/cases/identity_map_spec.rb
+++ spec/orm/cases/identity_map_spec.rb
@@ -1,19 +1,76 @@
 require 'spec_helper'
+require 'orm/models/friend'
+require 'orm/models/best_friend'
 
 describe MassiveRecord::ORM::IdentityMap do
-  describe "confirguration" do
+  before { MassiveRecord::ORM::IdentityMap.clear }
+
+  describe "class methods" do
     subject { described_class }
-    
-    describe ".enabled" do
-      context "when disabled" do
-        its(:enabled) { should be_false }
-        its(:enabled?) { should be_false }
+
+    describe "confirguration" do
+      describe ".enabled" do
+        context "when disabled" do
+          its(:enabled) { should be_false }
+          its(:enabled?) { should be_false }
+        end
+
+        context "when enabled" do
+          before { MassiveRecord::ORM::IdentityMap.enabled = true }
+          its(:enabled) { should be_true }
+          its(:enabled?) { should be_true }
+        end
+      end
+    end
+
+    describe "persistence" do
+      let(:person) { Person.new "id1" }
+      let(:friend) { Friend.new "id2" }
+
+      describe ".repository" do
+        its(:repository) { should eq Hash.new }
+
+        it "has values as a hash by default for any key" do
+          subject.repository['some_class'].should eq Hash.new
+        end
       end
 
-      context "when enabled" do
-        before { MassiveRecord::ORM::IdentityMap.enabled = true }
-        its(:enabled) { should be_true }
-        its(:enabled?) { should be_true }
+      describe ".clear" do
+        it "removes all values from repository" do
+          subject.repository['some_class']['an_id'] = Object.new
+          subject.clear
+          subject.repository.should be_empty
+        end
+      end
+
+      describe ".get" do
+        context "when it does not has the record" do
+          it "returns nil" do
+            subject.get(person.class, person.id).should be_nil
+          end
+        end
+
+        context "when it has the record" do
+          it "returns the record" do
+            subject.add person
+            subject.get(person.class, person.id).should eq person
+          end
+        end
+      end
+
+      describe ".add" do
+        it "persists the record" do
+          subject.add person
+          subject.repository[subject.send(:record_class_to_repository_key, person)][person.id].should eq person
+        end
+      end
+
+      describe ".remove" do
+        it "removes the record" do
+          subject.add person
+          subject.remove person
+          subject.get(person.class, person.id).should be_nil
+        end
       end
     end
   end
