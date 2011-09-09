81f11f985332510f7880c14c8ba1b4b3d049038b
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-09-09 14:44:26 +0200

    Persistence module now uses Operations::AtomicOperation.

--- lib/massive_record/orm/persistence.rb
+++ lib/massive_record/orm/persistence.rb
@@ -194,45 +194,8 @@ module MassiveRecord
       # is atomic. Thrift adapter is working with atomic decrementation.
       #
       def atomic_operation(operation, attr_name, by)
-        raise NotNumericalFieldError unless attributes_schema[attr_name.to_s].type == :integer
-
-        self.class.ensure_that_we_have_table_and_column_families!
-        attr_name = attr_name.to_s
-
-        ensure_proper_binary_integer_representation(attr_name)
-
-        self[attr_name] = row_for_record.send("atomic_#{operation}", attributes_schema[attr_name].unique_name, by)
-        @new_record = false
-        self[attr_name]
+        Operations.atomic_operation(self, :operation => operation, :attr_name => attr_name, :by => by).execute
       end
-
-      #
-      # To cope with the problem which arises when you ask to
-      # do atomic incrementation / decrementation of an attribute and that attribute
-      # has a string representation of a number, like "1", instead of
-      # the binary representation, like "\x00\x00\x00\x00\x00\x00\x00\x01".
-      #
-      # We then need to re-write that string representation into
-      # hex representation. Now, if you are on a completely new
-      # database and have never used MassiveRecord before we should not
-      # need to do this at all; numbers are now stored as hex, but for
-      # backward compatibility we are doing this.
-      #
-      # Now, there is a risk of doing this; if two calls are made to
-      # atomic_increment! or atomic_decrement! on a record where it's value is a string
-      # representation this operation might be compromised. Therefor
-      # you need to enable this feature.
-      #
-      def ensure_proper_binary_integer_representation(attr_name)
-        return if !backward_compatibility_integers_might_be_persisted_as_strings || new_record?
-
-        field = attributes_schema[attr_name]  
-
-        if raw_value = self.class.table.get(id, field.column_family.name, field.name)
-          store_record_to_database('update', [attr_name]) if raw_value =~ /\A\d*\Z/
-        end
-      end
-
     end
   end
 end
