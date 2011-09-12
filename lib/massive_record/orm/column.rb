297a453c9049bc73dd2268fdcecce09f27c1e059
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-09-12 16:27:09 +0200

    Added the deprecated Column class for backward compatibility.
    This commit may be reverted for upcommig release of MassiveRecord.

--- lib/massive_record/orm/column.rb
+++ lib/massive_record/orm/column.rb
@@ -1,9 +1,8 @@
-require 'massive_record/orm/column'
 require 'massive_record/orm/schema/embedded_interface'
 
 module MassiveRecord
   module ORM
-    class Embedded < Base
+    class Column < Base
       include Schema::EmbeddedInterface
 
       # TODO  Embedded does not support these kind of methods
@@ -14,6 +13,18 @@ module MassiveRecord
       undef_method :create, :reload, :save, :save!, :update_attribute, :update_attributes,
         :update_attributes!, :touch, :destroy, :increment!, :atomic_increment!,
         :decrement!, :delete
+
+
+
+      def self.inherited(by_class)
+        ActiveSupport::Deprecation.warn(
+          <<-TXT
+            #{by_class} inherits from MassiveRecord::ORM::Column which has been renamed to
+            MassiveRecord::ORM::Embedded. Please inherit from the Embedded class instead as
+            Column will be removed in the an upcomming of MassiveRecord.
+          TXT
+        )
+      end
     end
   end
 end
