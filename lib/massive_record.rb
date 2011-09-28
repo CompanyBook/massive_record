3097e057f825c11eb5e24ca792c13063b2dc0fa8
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-09-28 09:23:58 +0200

    Added RawData class. Is used to represent raw data.
    You can look at it as kinda the same as a
    Adapter::Thrift::Cell, but since we are gonna have
    more adapters, and I want something lightweight to
    carry this information (value and created at), I
    created this simple class.

--- lib/massive_record.rb
+++ lib/massive_record.rb
@@ -8,6 +8,7 @@ require 'massive_record/wrapper/base'
 
 # ORM
 require 'massive_record/orm/base'
+require 'massive_record/orm/raw_data'
 
 # Others
 if defined?(::Rails) && ::Rails::VERSION::MAJOR == 3
---
+++ lib/massive_record/orm/raw_data.rb
@@ -0,0 +1,41 @@
+module MassiveRecord
+  module ORM
+
+    #
+    # Class to hold one raw data, and some meta data for that value.
+    # As of writing this the meta data is when cell which the data
+    # originates from was last written to (created_at).
+    #
+    class RawData
+      attr_reader :value, :created_at
+
+
+      class << self
+        def new_with_data_from(object)
+          send("new_with_data_from_#{object.class.to_s.demodulize.underscore}", object)
+        end
+
+
+        private
+
+        def new_with_data_from_cell(cell)
+          new(value: cell.value, created_at: cell.created_at)
+        end
+      end
+
+
+
+      def initialize(attributes)
+        @value = attributes[:value]
+        @created_at = attributes[:created_at]
+      end
+
+
+
+      def inspect
+        "<#{self.class} #{value.inspect}>"
+      end
+      delegate :to_s, :to => :value
+    end
+  end
+end
