ee5b8265d8e9ce76683469fb377394ad345c3ea4
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-09-28 14:07:46 +0200

    Embedded records now have ability to reload.

--- lib/massive_record/orm/persistence/operations/embedded/reload.rb
+++ lib/massive_record/orm/persistence/operations/embedded/reload.rb
@@ -8,8 +8,28 @@ module MassiveRecord
           class Reload
             include Operations, OperationHelpers
 
+            class UnsupportedNumberOfEmbeddedIn < MassiveRecordError; end
+
             def execute
-              raise "NOT IMPLEMENTED YET!"
+              if record.persisted?
+                if embedded_in_proxies.compact.length == 1
+                  embeds_many_proxy_currently_embedded_in = inverse_proxy_for(embedded_in_proxies.first)
+                  embeds_many_proxy_currently_embedded_in.reload
+                  embeds_many_proxy_currently_embedded_in.find(record.id).tap do |reloaded_record|
+                    record.reinit_with({
+                      'attributes' => reloaded_record.attributes,
+                      'raw_data' => reloaded_record.raw_data
+                    })
+                  end
+                else
+                  raise UnsupportedNumberOfEmbeddedIn.new(<<-TXT
+                    Found '#{embedded_in_proxies.length}' embedded_in relations in #{klass}. We currently only support reload when embedded in '1' record
+                  TXT
+                  )
+                end
+
+                true
+              end
             end
           end
         end
--- spec/orm/persistence/operations/embedded/reload_spec.rb
+++ spec/orm/persistence/operations/embedded/reload_spec.rb
@@ -1,7 +1,7 @@
 
 require 'spec_helper'
 
-describe MassiveRecord::ORM::Persistence::Operations::Embedded::Insert do
+describe MassiveRecord::ORM::Persistence::Operations::Embedded::Reload do
   include SetUpHbaseConnectionBeforeAll 
   include SetTableNamesToTestTable
 
@@ -11,12 +11,57 @@ describe MassiveRecord::ORM::Persistence::Operations::Embedded::Insert do
   
   subject { described_class.new(record, options) }
 
+  before { record.person = person; record.save! }
+
   describe "generic behaviour" do
-    before { record.person = person }
     it_should_behave_like "a persistence embedded operation class"
   end
 
 
   describe "#execute" do
+    context "new record" do
+      before { record.stub(:persisted?).and_return false }
+
+      its(:execute) { should be_false }
+    end
+
+    context "persisted" do
+      let(:inverse_proxy) { mock(Object, :reload => true, :find => record) }
+      let(:embedded_in_proxy) { subject.embedded_in_proxies.first }
+
+      before do
+        subject.stub(:inverse_proxy_for).and_return(inverse_proxy)
+      end
+
+      it "raises error if unsupported numbers of embedded in relations" do
+        subject.should_receive(:embedded_in_proxies).any_number_of_times.and_return [1, 2]
+        expect { subject.execute }.to raise_error MassiveRecord::ORM::Persistence::Operations::Embedded::Reload::UnsupportedNumberOfEmbeddedIn
+      end
+
+      it "asks for inverse proxy" do
+        subject.should_receive(:inverse_proxy_for).with(embedded_in_proxy).and_return(inverse_proxy)
+        subject.execute
+      end
+
+      it "reloads inverse proxy" do
+        inverse_proxy.should_receive :reload
+        subject.execute
+      end
+
+      it "finds the record asked to be reloaded" do
+        inverse_proxy.should_receive(:find).with(record.id).and_return record
+        subject.execute
+      end
+
+      it "reinit record with found record's attributes and raw_data" do
+        record.should_receive(:attributes).and_return('attributes' => {})
+        record.should_receive(:raw_data).and_return('raw_data' => {})
+        record.should_receive(:reinit_with).with({
+          'attributes' => {'attributes' => {}},
+          'raw_data' => {'raw_data' => {}}
+        })
+        subject.execute
+      end
+    end
   end
 end
