563f0a4f724a503cd1f5cd998a9186c081346de9
Thorbjørn Hermansen <thhermansen@gmail.com>
2011-10-09 12:42:09 +0200

    Refactored callbacks test even more :-)

--- spec/orm/cases/callbacks_spec.rb
+++ spec/orm/cases/callbacks_spec.rb
@@ -3,33 +3,20 @@ require 'orm/models/callback_testable'
 require 'orm/models/person'
 require 'orm/models/address'
 
-#
-# Some basic tests
-#
-shared_examples_for "Massive Record with callbacks" do
-  it "should include ActiveModel::Callbacks" do
-    described_class.should respond_to :define_model_callbacks
+describe "callbacks" do
+  include SetUpHbaseConnectionBeforeAll
+  include SetTableNamesToTestTable
+
+  class CallbackDeveloperTable < MassiveRecord::ORM::Table
+    include CallbackTestable
+
+    column_family :base do
+      field :name, :default => "Thorbjorn"
+    end
   end
 
-  it "should include ActiveModell::Validations::Callback" do
-    described_class.included_modules.should include(ActiveModel::Validations::Callbacks)
-  end
-end
-
-[Person, Address].each do |klass|
-  describe klass do
-    it_should_behave_like "Massive Record with callbacks"
+  describe CallbackDeveloperTable do
+    it_should_behave_like "a model with callbacks"
   end
 end
 
-
-#
-# Some real life object tests
-#
-class CallbackDeveloperTable < MassiveRecord::ORM::Table
-  include CallbackTestable
-end
-
-describe CallbackDeveloperTable do
-  it_should_behave_like "a model with callbacks"
-end
