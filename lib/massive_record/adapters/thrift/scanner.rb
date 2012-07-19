32980d1f222b19bae9290343d2d995faecbd4ecf
Nathan Keyes <keyes@okcupidlabs.com>
2012-07-19 14:44:58 -0700

    * adding eclipse .project file to .gitignore * updating thrift dependency to 0.8.0 due to a bug in 0.6.0 (https://issues.apache.org/jira/browse/THRIFT-1400) * adding support for scanners with a prefix

--- lib/massive_record/adapters/thrift/scanner.rb
+++ lib/massive_record/adapters/thrift/scanner.rb
@@ -4,7 +4,7 @@ module MassiveRecord
       class Scanner
       
         attr_accessor :connection, :table_name, :column_family_names, :opened_scanner
-        attr_accessor :start_key, :offset_key, :created_at, :limit
+        attr_accessor :start_key, :start_prefix, :offset_key, :created_at, :limit
         attr_accessor :formatted_column_family_names, :column_family_names
       
         def initialize(connection, table_name, column_family_names, opts = {})
@@ -14,6 +14,7 @@ module MassiveRecord
           @column_family_names = opts[:columns] unless opts[:columns].nil?
           @formatted_column_family_names = @column_family_names.collect{|n| "#{n.split(":").first}:"}
           @start_key = opts[:start_key].to_s
+          @start_prefix = opts[:start_prefix].to_s
           @offset_key = opts[:offset_key].to_s
           @created_at = opts[:created_at].to_s
           @limit = opts[:limit] || 10
@@ -24,7 +25,9 @@ module MassiveRecord
         end
       
         def open
-          if created_at.empty?
+          if !start_prefix.empty?
+            self.opened_scanner = connection.scannerOpenWithPrefix(table_name, start_prefix, formatted_column_family_names)
+          elsif created_at.empty?
             self.opened_scanner = connection.scannerOpen(table_name, key, formatted_column_family_names)
           else
             self.opened_scanner = connection.scannerOpenTs(table_name, key, formatted_column_family_names, created_at)
--- massive_record.gemspec
+++ massive_record.gemspec
@@ -14,7 +14,7 @@ Gem::Specification.new do |s|
   s.rubyforge_project = "massive_record"
 
 
-  s.add_dependency "thrift", "= 0.6.0"
+  s.add_dependency "thrift", "~> 0.8.0"
   s.add_dependency "activesupport", "~> 3.0.7"
   s.add_dependency "activemodel", "~> 3.0.7"
   s.add_dependency "tzinfo"
