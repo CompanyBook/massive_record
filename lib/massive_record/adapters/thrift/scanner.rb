209ee52cb0c0042df6ca581e142d205f433911a2
Nathan Keyes <keyes@okcupidlabs.com>
2012-12-06 15:54:21 -0800

    [delivers #40811729]

--- lib/massive_record/adapters/thrift/scanner.rb
+++ lib/massive_record/adapters/thrift/scanner.rb
@@ -2,28 +2,28 @@ module MassiveRecord
   module Adapters
     module Thrift
       class Scanner
-      
+
         attr_accessor :connection, :table_name, :column_family_names, :opened_scanner
         attr_accessor :start_key, :start_prefix, :offset_key, :created_at, :limit
         attr_accessor :formatted_column_family_names, :column_family_names
-      
+
         def initialize(connection, table_name, column_family_names, opts = {})
           @connection = connection
           @table_name = table_name
-          @column_family_names = column_family_names.collect{|n| n.split(":").first}
+          @column_family_names = column_family_names.collect { |n| n.split(":").first }
           @column_family_names = opts[:columns] unless opts[:columns].nil?
-          @formatted_column_family_names = @column_family_names.collect{|n| "#{n.split(":").first}:"}
+          @formatted_column_family_names = @column_family_names.collect { |n| "#{n.split(":").first}:" }
           @start_key = opts[:start_key].to_s
           @start_prefix = opts[:start_prefix].to_s
           @offset_key = opts[:offset_key].to_s
           @created_at = opts[:created_at].to_s
           @limit = opts[:limit] || 10
         end
-      
+
         def key
           offset_key.empty? ? start_key : offset_key
         end
-      
+
         def open
           if !start_prefix.empty?
             self.opened_scanner = connection.scannerOpenWithPrefix(table_name, start_prefix, formatted_column_family_names)
@@ -33,29 +33,34 @@ module MassiveRecord
             self.opened_scanner = connection.scannerOpenTs(table_name, key, formatted_column_family_names, created_at)
           end
         end
-      
+
         def close
-          connection.scannerClose(opened_scanner)
+          begin
+            connection.scannerClose(opened_scanner)
+          rescue Apache::Hadoop::Hbase::Thrift::IllegalArgument => e
+            #TODO log this
+            raise e unless e.message =~ /scanner ID is invalid/
+          end
         end
-      
+
         def fetch_trows(opts = {})
           connection.scannerGetList(opened_scanner, limit)
         end
-      
+
         def fetch_rows(opts = {})
           populate_rows(fetch_trows(opts))
         end
-      
+
         def populate_rows(results)
           results.collect do |result|
             populate_row(result) unless result.row.match(/^#{start_key.gsub("|", "\\|")}/).nil?
-          end.select{|r| !r.nil?}
+          end.select { |r| !r.nil? }
         end
-      
+
         def populate_row(result)
           Row.populate_from_trow_result(result, connection, table_name, column_family_names)
         end
-      
+
       end
     end
   end
