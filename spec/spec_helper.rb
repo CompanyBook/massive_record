bb9e2faa10f5647e18488df3e3be35b703a88243
Nathan Keyes <keyes@okcupidlabs.com>
2013-03-24 17:24:50 -0700

    spec helper refactor [#46676935]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'support'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'shared'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'orm', 'models'))
+
 require 'rubygems'
 require 'bundler'
 require 'yaml'
+require 'massive_record'
 
 Bundler.require :default, :development
 
@@ -11,11 +18,12 @@ RSpec.configure do |c|
   #c.fail_fast = true 
 end
 
-Dir["#{SPEC_DIR}/orm/models/*.rb"].each { |f| require f }
-Dir["#{SPEC_DIR}/support/**/*.rb"].each { |f| require f }
+
 Dir["#{SPEC_DIR}/shared/**/*.rb"].each { |f| require f }
+Dir["#{SPEC_DIR}/support/**/*.rb"].each { |f| require f }
+Dir["#{SPEC_DIR}/orm/models/*.rb"].each { |f| require f }
 
-
+require 'massive_record/orm/identity_map'
 MassiveRecord::ORM::IdentityMap.enabled = false
 if MassiveRecord::ORM::IdentityMap.enabled?
   RSpec.configure do |c|
